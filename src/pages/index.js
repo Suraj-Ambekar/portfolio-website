e632e2a06b8aab96cef7a40355badfa7ea41fb4f
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-06-18 23:50:26 +0530

    created basic structure of home section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Layout from '@/components/Layout';
 import Head from 'next/head';
 
 export default function Home() {
@@ -7,8 +8,12 @@ export default function Home() {
                 <title>Suraj Ambekar - Portfolio website</title>
                 <meta name="description" content="portfolio website of Suraj Ambekar" />
             </Head>
-            <main className="">
-                Home hi
+            <main className="flex items-center text-dark w-full min-h-screen">
+                <Layout>
+                    <div className='flex items-center justify-between w-full'>
+                        <Image src="" alt="Suraj Ambekar" className="w-full h-auto" />
+                    </div>
+                </Layout>
             </main>
         </>
         
