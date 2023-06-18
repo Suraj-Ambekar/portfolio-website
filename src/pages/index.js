be0634b08f7f15c1053093f377d221024fd20a31
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-06-19 02:25:04 +0530

    added new image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import Layout from '@/components/Layout';
 import Head from 'next/head';
+import Image from 'next/image';
+import profilePic from "../pages/new.png";
 
 export default function Home() {
     return (
@@ -9,9 +11,12 @@ export default function Home() {
                 <meta name="description" content="portfolio website of Suraj Ambekar" />
             </Head>
             <main className="flex items-center text-dark w-full min-h-screen">
-                <Layout>
+                <Layout className='pt-0'>
                     <div className='flex items-center justify-between w-full'>
-                        <Image src="" alt="Suraj Ambekar" className="w-full h-auto" />
+                        <div className='w-1/2'>
+                            <Image src={profilePic} alt="Suraj Ambekar" className="w-full h-auto" />
+                        </div>
+                        
                     </div>
                 </Layout>
             </main>
