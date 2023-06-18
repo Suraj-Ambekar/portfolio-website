54b7bc5e12467b561b2d52c5de2fc92667e7849b
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-06-18 12:29:42 +0530

    created basic structure of navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import Link from 'next/link';
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
+      <nav>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/projects">Projects</Link>
+        <Link href="/articles">Articles</Link>
+      </nav>
+      <h2>Logo</h2>
+      <nav>
+        <Link href="/" target={"_blank"}>GitHub</Link>
+        <Link href="/about" target={"_blank"}>LinkedIn</Link>
+        <Link href="/projects" target={"_blank"}>Instagram</Link>
+        <Link href="/articles" target={"_blank"}>Whatsapp</Link>
+      </nav>
+    </header>
   )
 }
 
