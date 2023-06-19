7d47ea78837b4834adb6d1360c997347c655ae05
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-06-19 08:10:26 +0530

    added text and animated it

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Layout from '@/components/Layout';
 import Head from 'next/head';
 import Image from 'next/image';
 import profilePic from "../pages/new.png";
+import AnimatedText from '@/components/AnimatedText';
 
 export default function Home() {
     return (
@@ -16,7 +17,10 @@ export default function Home() {
                         <div className='w-1/2'>
                             <Image src={profilePic} alt="Suraj Ambekar" className="w-full h-auto" />
                         </div>
-                        
+                        <div className='w-1/2 flex flex-col items-center self-center'>
+                            <AnimatedText text="Turning Vision Into Reality With Code And Design." className='!text-6xl !text-left' />
+                            <p>As a skilled front-end developer, I am dedicated to turning ideas into innovative web applications. Explore my latest projects and articles, showcasing my expertise in React.js and web development.</p>
+                        </div>
                     </div>
                 </Layout>
             </main>
