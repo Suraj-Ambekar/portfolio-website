b1d0422ba601269e291fee08f259b717a2ecc228
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-06-19 17:48:40 +0530

    added bulb image on home

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import AnimatedText from '@/components/AnimatedText';
 import Link from 'next/link';
 import { LinkArrow } from '@/components/Icons';
 import HireMe from '@/components/HireMe';
+import lightBulb from "../../public/images/bulb.svg";
 
 export default function Home() {
     return (
@@ -43,6 +44,10 @@ export default function Home() {
                 </Layout>
 
                 <HireMe />
+
+                <div className='absolute right-8 bottom-8 inline-block w-24'>
+                    <Image src={lightBulb} alt="lightbulb" className='w-full h-auto'  />
+                </div>
             </main>
         </>
         
