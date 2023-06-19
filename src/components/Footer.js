05d54b0d8f4160e8ec91124bc2ef9c9bd3179c2a
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-06-19 15:50:43 +0530

    completed footer section

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Layout from './Layout';
+import Link from 'next/link';
+
+const Footer = () => {
+  return (
+    <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg">
+        <Layout className='py-8 flex items-center justify-between'>
+            <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
+            <div className='flex items-center'>
+                Build Width <span className='text-primary text-2xl px-1'>&#9825;</span>
+                by&nbsp; <Link href="https://www.linkedin.com/in/suraj-ambekar/" className="underline underline-offset-2" target={"_blank"}>Suraj Ambekar</Link>
+            </div>
+            <Link href="https://www.linkedin.com/in/suraj-ambekar/" target={"_blank"} className="underline underline-offset-2">Say hello</Link>
+        </Layout>
+    </footer>
+  )
+}
+
+export default Footer;
