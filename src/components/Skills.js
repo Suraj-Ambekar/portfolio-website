af6ea108d89f0481ca6181c0bdce2b16b0ed15af
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-06-21 16:20:50 +0530

    completed skills sections

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,20 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 
+const Skill = ({name, x, y}) => {
+  return (
+    <motion.div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light py-3 px-6 shadow-dark cursor-pointer absolute'
+      whileHover={{scale: 1.1}}   
+      initial={{x:0, y:0}}
+      whileInView={{x:x,y:y}}
+      transition={{duration: 1.5}}
+      viewport={{once: true}}
+    >
+      {name}
+    </motion.div>
+  )
+}
+
 const Skills = () => {
   return (
     <>
@@ -11,7 +25,20 @@ const Skills = () => {
         >
           Web
         </motion.div>
+
+        <Skill name="HTML" x="0vw" y="-11vw" />
+        <Skill name="CSS" x="-20vw" y="0vw" />
+        <Skill name="JavaScript" x="20vw" y="0vw" />
+        <Skill name="ReactJs" x="-0vw" y="11vw" />
+        <Skill name="NextJs" x="19vw" y="-10vw" />
+        <Skill name="Redux" x="-19vw" y="-10vw" />
+        <Skill name="Bootstrap" x="-19vw" y="10vw" />
+        <Skill name="TailwindCSS" x="19vw" y="10vw" />
+        <Skill name="SCSS" x="0vw" y="-19vw" />
+        <Skill name="Git" x="-33vw" y="0vw" />
+        <Skill name="GitHub" x="33vw" y="0vw" />
       </div>
+
     </>
   )
 }
