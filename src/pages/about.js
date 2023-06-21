376d1746effcb1558276de65aa62140beed66c53
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-06-21 13:40:39 +0530

    completed last section of first part of about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import Layout from '@/components/Layout'
 import Head from 'next/head'
 import React from 'react'
 import Image from 'next/image'
-import profilePic from "../../public/images/IMG_8076.jpg";
+import profilePic from "../../public/images/IMG_8077.jpg";
 
 const about = () => {
   return (
@@ -15,7 +15,7 @@ const about = () => {
         <main className='flex w-full flex-col items-center justify-center'>
             <Layout className='py-16'>
                 <AnimatedText text="Passion Fuels Purpose!" className='mb-16'/>
-                <div className='grid w-full grid-cols-6 gap-16'>
+                <div className='grid w-full grid-cols-8 gap-16'>
                     <div className='col-span-3 flex flex-col items-start justify-start '>
                         <h2 className='mb-4 text-lg font-bold uppercase text-dark/75'>About Me</h2>
                         <p className='font-medium'>
@@ -31,6 +31,26 @@ const about = () => {
                     <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8'>
                         <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark' />
                         <Image src={profilePic} alt="Suraj Ambekar" className="w-full h-auto rounded-2xl" />                        
+                    </div>
+                    <div className='col-span-2 flex flex-col items-end justify-between'>
+                        <div className='flex flex-col items-end justify-center'>
+                            <span className='inline-block text-7xl font-bold'>
+                                1
+                            </span>
+                            <h2 className='text-xl font-medium capitalize text-dark/75'>Aschievements</h2>
+                        </div>
+                        <div className='flex flex-col items-end justify-center'>
+                            <span className='inline-block text-7xl font-bold'>
+                                10+
+                            </span>
+                            <h2 className='text-xl font-medium capitalize text-dark/75'>Projects Completed</h2>
+                        </div>
+                        <div className='flex flex-col items-end justify-center'>
+                            <span className='inline-block text-7xl font-bold'>
+                                0
+                            </span>
+                            <h2 className='text-xl font-medium capitalize text-dark/75'>Years of experience</h2>
+                        </div>
                     </div>
                 </div>
             </Layout>
