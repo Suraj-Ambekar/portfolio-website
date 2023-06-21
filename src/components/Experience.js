3013f012adccbf10e7132187eb6a2a1a9b7a504f
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-06-21 17:25:55 +0530

    added scrolling effect to circle

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,8 +1,13 @@
 import React, { useRef } from 'react'
 import {motion, useScroll} from "framer-motion"
+import Lilcon from './Lilcon'
 
 const Details = ({position, company, comapnyLink, time, address, work}) => {
-    return <li className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
+
+    const ref = useRef(null);
+
+    return <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
+         <Lilcon reference={ref} />
         <div>
             <h3 className='capitalize font-bold text-2xl'>{position}&nbsp;<a href={comapnyLink} target='_blank' className='text-primary capitalize'>{company}</a></h3>
             <span className='capitalize font-medium text-dark/75'>
--- src/components/Lilcon.js
+++ src/components/Lilcon.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { motion, useScroll } from 'framer-motion'
 
-const Lilcon = () => {
+const Lilcon = ({reference}) => {
 
     const {scrollYProgress} =useScroll(
         {
-            target: ref,
-            offset: ["start end", "center start"]
+            target: reference,
+            offset: ["center end", "center center"]
         }
     )
 
@@ -14,7 +14,9 @@ const Lilcon = () => {
     <figure className='absolute left-0 stroke-dark'>
         <svg width="75" height="75" viewBox='0 0 100 100'>
             <circle cx="75" cy="50" r="20" className='stroke-primary stroke-1 fill-none'/>
-            <motion.circle cx="75" cy="50" r="20" className='stroke-[5px] fill-light'/>
+            <motion.circle cx="75" cy="50" r="20" className='stroke-[5px] fill-light' 
+                style={{pathLength: scrollYProgress}}
+            />
             <circle cx="75" cy="50" r="10" className='stroke-1 fill-primary'/>
         </svg>
     </figure>
