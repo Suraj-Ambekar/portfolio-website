8c48d5f18d4fdcee48a523645f11820d66d555db
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-06-21 17:56:21 +0530

    added scrolling effect to position and completed experience section

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -4,11 +4,15 @@ import Lilcon from './Lilcon'
 
 const Details = ({position, company, comapnyLink, time, address, work}) => {
 
-    const ref = useRef(null);
+    const reff = useRef(null);
 
-    return <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
-         <Lilcon reference={ref} />
-        <div>
+    return <li ref={reff} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
+        <Lilcon reference={reff} />
+        <motion.div
+            initial={{y:50}}
+            whileInView={{y:0}}
+            transition={{duration:0.5, type:"spring"}}
+        >
             <h3 className='capitalize font-bold text-2xl'>{position}&nbsp;<a href={comapnyLink} target='_blank' className='text-primary capitalize'>{company}</a></h3>
             <span className='capitalize font-medium text-dark/75'>
                 {time}  {address}
@@ -16,7 +20,7 @@ const Details = ({position, company, comapnyLink, time, address, work}) => {
             <p className='font-medium w-full'>
                 {work}
             </p>
-        </div>
+        </motion.div>
     </li>
 }
 
@@ -35,7 +39,7 @@ const Experience = () => {
             Experience
         </h2>
         <div className='w-[75%] mx-auto relative'>
-            <motion.div className='absolute left-8 top-0 w-[4px] h-full bg-dark origin-top' ref={ref} 
+            <motion.div className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top' ref={ref} 
                 style={{scaleY: scrollYProgress}}
             />
             <ul className='w-full flex flex-col items-start justify-between ml-4'>
