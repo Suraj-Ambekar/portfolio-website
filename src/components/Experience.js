7a53f9cf650ea31a184f3d3eeeaf68eb2aac62b4
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-06-21 17:11:10 +0530

    added scroll effect

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import {motion, useScroll} from "framer-motion"
 
 const Details = ({position, company, comapnyLink, time, address, work}) => {
     return <li className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
@@ -15,12 +16,23 @@ const Details = ({position, company, comapnyLink, time, address, work}) => {
 }
 
 const Experience = () => {
+    const ref = useRef(null);
+
+    const {scrollYProgress} =useScroll(
+        {
+            target: ref,
+            offset: ["start end", "center start"]
+        }
+    )
   return (
     <div className='my-64'>
         <h2 className='font-bold text-8xl mb-32 w-full text-center'>
             Experience
         </h2>
         <div className='w-[75%] mx-auto relative'>
+            <motion.div className='absolute left-8 top-0 w-[4px] h-full bg-dark origin-top' ref={ref} 
+                style={{scaleY: scrollYProgress}}
+            />
             <ul className='w-full flex flex-col items-start justify-between ml-4'>
                 <Details 
                     position="" company="" comapnyLink="" time="" address="" work=""
