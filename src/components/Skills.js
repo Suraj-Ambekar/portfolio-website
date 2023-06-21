f337c3eaaaf61fd1d2e8d637a837448815186e37
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-06-21 15:51:51 +0530

    added hover effect on web

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <>
+      <h2 className='font-bold text-8xl mt-64 w-full text-center'>Skills</h2>
+      <div className='w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight'>
+        <motion.div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark cursor-pointer'
+          whileHover={{scaleX: 1.5, scale: 1.2}}   
+        >
+          Web
+        </motion.div>
+      </div>
+    </>
   )
 }
 
