5e65ffc3d9b3ae63af73c64a72437db596a5624d
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-06-21 10:59:04 +0530

    started working on about page

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import React from 'react'
+
+const about = () => {
+  return (
+    <>
+        <Head>
+            <title>Suraj Ambekar | About Page</title>
+            <meta name="description" content="About page" />
+        </Head>
+    </>
+  )
+}
+
+export default about
