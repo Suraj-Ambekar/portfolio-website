5f9f738ca3522f0eee4857243725ca88a89bce30
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-06-22 18:19:22 +0530

    created basic layout for projects columns

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,3 +1,6 @@
+import AnimatedText from '@/components/AnimatedText'
+import Layout from '@/components/Layout'
+import Head from 'next/head'
 import React from 'react'
 
 const projects = () => {
@@ -7,8 +10,24 @@ const projects = () => {
             <title>Suraj Ambekar | Projects Page</title>
             <meta name="description" content="Projects page" />
         </Head>
-        <main>
-            
+        <main className='w-full mb-16 flex flex-col items-center justify-center'>
+            <Layout className='pt-16' >
+                <AnimatedText text="Imagination Trumps knowledge!" />
+                <div className='grid grid-cols-12 gap-24'>
+                    <div className='col-span-12'>
+                        Featured Project
+                    </div>
+                    <div className='col-span-6'>
+                        Project-1
+                    </div>
+                    <div className='col-span-6'>
+                        Project-2
+                    </div>
+                    <div className='col-span-12'>
+                        Project-3
+                    </div>
+                </div>
+            </Layout>
         </main>
     </>
   )
