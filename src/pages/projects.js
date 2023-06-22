d009b56fae1cef75e3302958ca90daf801cdec2c
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-06-22 19:39:09 +0530

    added styling to featured component

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -9,19 +9,21 @@ import project1 from '../../public/images/projects images/project1.JPG';
 
 const FeaturedProject = ({type, title, summary, img, link, github}) => {
     return (
-        <article>
-            <Link href={link} target="_blank">
+        <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12">
+            <Link className='w-1/2 cursor-pointer overflow-hidden rounded-lg' href={link} target="_blank">
                 <Image src={img} alt={title} className='w-full h-auto' />
             </Link>
-            <div>
-                <span>{type}</span>
-                <Link href={link} target="_blank">
-                    <h2>{title}</h2>
+            <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
+                <span className='text-primary font-medium text-xl'>{type}</span>
+                <Link href={link} target="_blank" className='hover:underline underline-offset-2'>
+                    <h2 className='my-2 w-full text-left text-4xl font-bold'>{title}</h2>
                 </Link>
-                <p>{summary}</p>
-                <div>
-                    <Link href={github} target="_blank"><GitHub /></Link>
-                    <Link href={link} target="_blank">Demo</Link>
+                <p className='my-2 font-medium text-dark'>{summary}</p>
+                <div className='mt-2 flex items-center'>
+                    <Link href={github} target="_blank" className='w-10'><GitHub /></Link>
+                    <Link href={link} target="_blank" 
+                        className='ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold '
+                    >Visit Project</Link>
                 </div>
             </div>
         </article>
@@ -38,13 +40,13 @@ const projects = () => {
         </Head>
         <main className='w-full mb-16 flex flex-col items-center justify-center'>
             <Layout className='pt-16' >
-                <AnimatedText text="Imagination Trumps knowledge!" />
+                <AnimatedText text="Imagination Trumps knowledge!"  className='mb-16' />
                 <div className='grid grid-cols-12 gap-24'>
                     <div className='col-span-12'>
                         <FeaturedProject 
                             title="CineMagic: A Movie Rating Website"
                             img={project1}
-                            summary="A feature-A Movie Rating Website using React, SCSS, and TMDB (database api) . It shows detail regarding almost all the movies. You can easily search any movie and apply filter to search specific genre movies."
+                            summary="A feature-A Movie Rating Website using React, SCSS, and TMDB (database api) . It shows details regarding almost all the movies. You can easily search any movie and apply filter to search specific genre movies."
                             link="https://cinemagic-website.netlify.app/"
                             github="https://github.com/Suraj-Ambekar/CineMagic"
                             type="Featured Project"
