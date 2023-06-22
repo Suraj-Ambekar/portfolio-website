6d395882f9a61d56eb9d8bfc99d981e9fefdfaa6
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-06-22 18:03:57 +0530

    started working with projects page

---
+++ src/pages/projects.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const projects = () => {
+  return (
+    <>
+        <Head>
+            <title>Suraj Ambekar | Projects Page</title>
+            <meta name="description" content="Projects page" />
+        </Head>
+        <main>
+            
+        </main>
+    </>
+  )
+}
+
+export default projects
