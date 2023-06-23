009ff07f8e3bfa70a776bde08e205bbcb3e0aa5e
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-06-23 12:41:39 +0530

    added basic structure of articles

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -7,12 +7,17 @@ import React from 'react'
 import { motion } from 'framer-motion'
 import article1 from "../../public/images/article images/article1.jpg"
 import article2 from "../../public/images/article images/article2.jpg"
+import article3 from "../../public/images/article images/article3.jpg"
+import article4 from "../../public/images/article images/article4.jpg"
+import article5 from "../../public/images/article images/article5.png"
+import article6 from "../../public/images/article images/article6.png"
 
 const FramerImage = motion(Image)
 
 const FeaturedArticle = ({img,title, time, summary, link}) => {
     return (
-        <li className='col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl'>
+        <li className='relative col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl'>
+            <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-red-900 rounded-br-3xl' />
             <Link
                 href={link}
                 target="_blank"
@@ -24,10 +29,21 @@ const FeaturedArticle = ({img,title, time, summary, link}) => {
                 />
             </Link>
             <Link href={link} target='_blank'>
-                <h2 className='capitalize text-2xl font-bold my-2 hover:underline'>{title}</h2>
+                <h2 className='capitalize text-2xl font-bold my-2 mt-4 hover:underline'>{title}</h2>
             </Link>
             <p className='text-sm mb-2'>{summary}</p>
             <span className='text-primary font-semibold'>{time}</span>
+        </li>
+    )
+}
+
+const Article = ({img, title, date, link}) => {
+    return (
+        <li>
+            <Link href={link} target="_blank">
+                <h2>{title}</h2>
+            </Link>
+            <span>{date}</span>
         </li>
     )
 }
@@ -56,6 +72,33 @@ const articles = () => {
                         time="6 min read"
                         link="https://www.showwcase.com/show/18714/12-steps-you-need-to-take-before-deploying-your-website"
                         img={article2}
+                    />
+                </ul>
+                <h2 className='font-bold text-4xl w-full text-center my-16 mt-32'>All Articles</h2>
+                <ul>
+                    <Article 
+                        title="Introduction to JavaScript"
+                        date="17 December, 2022"
+                        link="https://www.showwcase.com/show/19402/introduction-to-javascript"
+                        img={article3}
+                    />
+                    <Article 
+                        title="How to create a Responsive Website"
+                        date="9 December, 2022"
+                        link="https://www.showwcase.com/show/19194/how-to-create-a-responsive-website"
+                        img={article4}
+                    />
+                    <Article 
+                        title="Free Resources For Your Web Development Career"
+                        date="3 December, 2022"
+                        link="https://www.showwcase.com/show/18962/free-resources-for-your-web-development-career"
+                        img={article5}
+                    />
+                    <Article 
+                        title="Step-by-step breakdown of a resume"
+                        date="26 January, 2023"
+                        link="https://www.showwcase.com/show/30423/step-by-step-breakdown-of-a-resume-that-got-shortlisted-by-google-microsoft-and-amazon"
+                        img={article6}
                     />
                 </ul>
             </Layout>
