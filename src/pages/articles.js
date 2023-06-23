5767b5e8ff4340098c95c77dc12fac1f8d42bde6
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-06-23 15:20:13 +0530

    styled article component

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -4,13 +4,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import { motion } from 'framer-motion'
+import { motion, useMotionValue } from 'framer-motion'
 import article1 from "../../public/images/article images/article1.jpg"
 import article2 from "../../public/images/article images/article2.jpg"
 import article3 from "../../public/images/article images/article3.jpg"
 import article4 from "../../public/images/article images/article4.jpg"
 import article5 from "../../public/images/article images/article5.png"
 import article6 from "../../public/images/article images/article6.png"
+import { useRef } from 'react'
 
 const FramerImage = motion(Image)
 
@@ -37,14 +38,46 @@ const FeaturedArticle = ({img,title, time, summary, link}) => {
     )
 }
 
+const MovingImg = ({title, img, link}) => {
+
+    const x = useMotionValue(0);
+    const y = useMotionValue(0);
+    const imgRef = useRef(null);
+    
+    function handleMouse(event) {
+        imgRef.current.style.display = "inline-block";
+        x.set(event.pageX);
+        y.set(-10);
+    }
+
+    function handleMouseLeave(event) {
+        imgRef.current.style.display = "none";
+        x.set(0);
+        y.set(0);
+    }
+
+    return (
+        <Link href={link} target='_blank'
+            onMouseMove={handleMouse}
+            onMouseLeave={handleMouseLeave}
+        >
+            <h2 className='capitalize text-xl font-semibold hover:underline'>
+                {title}
+            </h2>
+            <FramerImage style={{x:x, y:y}} ref={imgRef} src={img} alt={title} className='z-10 w-96 h-auto hidden absolute rounded-lg' initial={{opacity:0}} whileInView={{opacity:1, transition: {duration: 0.2}}} />
+        </Link>
+    )
+}
+
 const Article = ({img, title, date, link}) => {
     return (
-        <li className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark">
-            <Link href={link} target="_blank">
-                <h2>{title}</h2>
-            </Link>
-            <span>{date}</span>
-        </li>
+        <motion.li className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4"
+            initial={{y:200}}
+            whileInView={{y:0, transition: {duration:0.5, ease:"easeInOut"}}}
+        >
+            <MovingImg title={title} img={img} link={link} />
+            <span className='text-primary font-semibold pl-4'>{date}</span>
+        </motion.li>
     )
 }
 
