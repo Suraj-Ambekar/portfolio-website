fd7d8ceceb3a489e749e4b6ac77de5daff4c7a57
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-06-23 10:52:24 +0530

    added hover effect on images

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -4,20 +4,27 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import article1 from "../../public/images/article images/article1.JPG"
+import { motion } from 'framer-motion'
+import article1 from "../../public/images/article images/article1.jpg"
+import article2 from "../../public/images/article images/article2.jpg"
+
+const FramerImage = motion(Image)
 
 const FeaturedArticle = ({img,title, time, summary, link}) => {
     return (
-        <li>
+        <li className='col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl'>
             <Link
                 href={link}
                 target="_blank"
-                className='w-full cursor-pointer overflow-hidden rounded-lg'
+                className='w-full inline-block cursor-pointer overflow-hidden rounded-lg'
             >
-                <Image src={img} alt={title} className='w-full h-auto'/>
+                <FramerImage src={img} alt={title} className='w-full h-auto'
+                    whileHover={{scale: 1.05}}
+                    transition={{duration:0.2}}
+                />
             </Link>
             <Link href={link} target='_blank'>
-                <h2 className='capitalize text-2xl font-bold my-2'>{title}</h2>
+                <h2 className='capitalize text-2xl font-bold my-2 hover:underline'>{title}</h2>
             </Link>
             <p className='text-sm mb-2'>{summary}</p>
             <span className='text-primary font-semibold'>{time}</span>
@@ -43,7 +50,13 @@ const articles = () => {
                         link="https://www.showwcase.com/show/18485/how-to-become-a-front-end-developer"
                         img={article1}
                     />
-                    <li>Featured articles-2</li>
+                    <FeaturedArticle 
+                        title="12 Steps You Need To Take Before Deploying Your Website"
+                        summary="When you are finished developing a website and you have done testing, now the biggest part comes that you want to make your website live in search engines. For that, you have to deploy your website. Deploying a website looks easy but you have to check some steps before deploying your website to make sure everything is fine."
+                        time="6 min read"
+                        link="https://www.showwcase.com/show/18714/12-steps-you-need-to-take-before-deploying-your-website"
+                        img={article2}
+                    />
                 </ul>
             </Layout>
         </main>
