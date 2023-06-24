b97e877593256960abdaced4dd4e17b487f8d398
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-06-24 22:34:33 +0530

    added two new tailwind variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,9 @@ module.exports = {
       },
       backgroundImage: {
         circularLight: 'repeating-radial-gradient(rgba(0,0,0,0.4),2px, #f5f5f5 5px, #f5f5f5 100px);',
-        circularDark: 'repeating-radial-gradient(rgba(255,255,255,0.5),2px, #1b1b1b 5px, #1b1b1b 100px);'
+        circularDark: 'repeating-radial-gradient(rgba(255,255,255,0.5),2px, #1b1b1b 5px, #1b1b1b 100px);',
+        circularLightLg: 'repeating-radial-gradient(rgba(0,0,0,0.4),2px, #f5f5f5 5px, #f5f5f5 80px);',
+        circularDarkLg: 'repeating-radial-gradient(rgba(255,255,255,0.5),2px, #1b1b1b 5px, #1b1b1b 80px);',
       }
     },
     screens: {
