510a95cc47de7c9027d88a06a5400c25d71b2711
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-06-24 23:00:15 +0530

    made it responsive

--- src/components/Education.js
+++ src/components/Education.js
@@ -6,18 +6,18 @@ const Details = ({type, time, place, info}) => {
 
     const reff = useRef(null);
 
-    return <li ref={reff} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-start justify-between'>
+    return <li ref={reff} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-start justify-between md:w-[80%]'>
         <Lilcon reference={reff} />
         <motion.div
             initial={{y:50}}
             whileInView={{y:0}}
             transition={{duration:0.5, type:"spring"}}
         >
-            <h3 className='capitalize font-bold text-2xl'>{type}</h3>
-            <span className='capitalize font-medium text-dark/75 dark:text-light/75'>
+            <h3 className='capitalize font-bold text-2xl sm:text-xl xs:text-lg'>{type}</h3>
+            <span className='capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm'>
                 {time} | {place}
             </span>
-            <p className='font-medium w-full'>
+            <p className='font-medium w-full md:text-sm'>
                 {info}
             </p>
         </motion.div>
@@ -35,14 +35,14 @@ const Education = () => {
     )
   return (
     <div className='my-64'>
-        <h2 className='font-bold text-8xl mb-32 w-full text-center'>
+        <h2 className='font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16'>
             Education
         </h2>
-        <div className='w-[75%] mx-auto relative'>
-            <motion.div className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light' ref={ref} 
+        <div className='w-[75%] mx-auto relative lg:w-[90%] md:w-full'>
+            <motion.div className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light md:w-[2px] md:left-[30px] xs:left-[20px]' ref={ref} 
                 style={{scaleY: scrollYProgress}}
             />
-            <ul className='w-full flex flex-col items-start justify-between ml-4'>
+            <ul className='w-full flex flex-col items-start justify-between ml-4 xs:ml-2'>
                 <Details 
                     type="Master Of Science In Information Technology"
                     time="2021-2023"
