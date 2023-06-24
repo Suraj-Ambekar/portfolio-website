6859787a5f13c921ab200baa2af2d3077fa5d233
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-06-24 15:30:55 +0530

    added animation on moblie menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -115,9 +115,14 @@ const NavBar = () => {
 
       {
         isOpen ?
-          <div className='min-w-[70vw] flex flex-col justify-between items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-30
-          bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32
-          '>
+          <motion.div 
+            initial={{scale:0, opacity: 0, x:"-50%", y: "-50%"}}
+            animate={{scale:1, opacity:1}}
+          
+            className='min-w-[70vw] flex flex-col justify-between items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-30
+            bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32
+          '
+          >
             <nav className='flex items-center flex-col justify-center'>
             <CustomMobileLink href="/" title="Home" className='' toggle={handleClick} />
             <CustomMobileLink href="/about" title="About" className='' toggle={handleClick} />
@@ -165,7 +170,7 @@ const NavBar = () => {
               }
             </button>
             </nav>
-          </div>
+          </motion.div>
 
         : null
       }
