7de417c9e9db14564f8d4c2bc6b19da63d47d0e6
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-06-24 17:04:50 +0530

    added media queries to make page responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,10 +16,10 @@ export default function Home() {
                 <meta name="description" content="portfolio website of Suraj Ambekar" />
             </Head>
             <main className="flex items-center text-dark w-full min-h-screen dark:text-light">
-                <Layout className='pt-0'>
-                    <div className='flex items-center justify-between w-full'>
-                        <div className='w-1/2'>
-                            <Image src={profilePic} alt="Suraj Ambekar" className="w-full h-auto" 
+                <Layout className='pt-0 md:pt-16 sm:pt-8'>
+                    <div className='flex items-center justify-between w-full lg:flex-col'>
+                        <div className='w-1/2 md:w-full'>
+                            <Image src={profilePic} alt="Suraj Ambekar" className="w-full h-auto lg:hidden md:inline-block md:w-full" 
                                 priority
                                 sizes="(max-width: 768px) 100vw,
                                         (max-width: 1200px) 50vw,
@@ -27,22 +27,25 @@ export default function Home() {
                                 "
                             />
                         </div>
-                        <div className='w-1/2 flex flex-col items-center self-center'>
-                            <AnimatedText text="Turning Vision Into Reality With Code And Design." className='!text-6xl !text-left' />
-                            <p className='my-4 text-base font-medium'>
+                        <div className='w-1/2 flex flex-col items-center self-center lg:w-full'>
+                            <AnimatedText text="Turning Vision Into Reality With Code And Design." 
+                                className='!text-6xl !text-left xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl' 
+                            />
+                            <p className='my-4 text-base font-medium md:text-sm sm:text-xs'>
                                 As a skilled front-end developer, I am dedicated to turning ideas into innovative web applications. Explore my latest projects and articles, showcasing my expertise in React.js and web development.
                             </p>
-                            <div className='flex items-center self-start mt-2'>
+                            <div className='flex items-center self-start mt-2 lg:self-center'>
                                 <Link href="/SurajAmbekar-Resume.pdf" target={"_blank"} 
                                     className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
                                         border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light
+                                        md:p-2 md:px-4 md:text-base
                                     '
                                     download={true}
                                 >
                                     Resume <LinkArrow className={"w-6 ml-1"} />
                                 </Link>
                                 <Link href="mailto:ambekarsuraj03@gmail.com" target={"_blank"} 
-                                    className='ml-4 text-lg font-medium capitalize text-dark underline dark:text-light'
+                                    className='ml-4 text-lg font-medium capitalize text-dark underline dark:text-light md:text-base'
                                 >Contact</Link>
                             </div>
                         </div>
@@ -51,7 +54,7 @@ export default function Home() {
 
                 <HireMe />
 
-                <div className='absolute right-8 bottom-8 inline-block w-24'>
+                <div className='absolute right-8 bottom-8 inline-block w-24 md:hidden'>
                     <Image src={lightBulb} alt="lightbulb" className='w-full h-auto'  />
                 </div>
             </main>
