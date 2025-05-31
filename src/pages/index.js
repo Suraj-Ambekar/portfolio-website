e274dfbbacd58e82b15fc65ceedcbc61a1d3560a
Suraj Ambekar <ambekarsuraj03@gmail.com>
2025-05-31 18:39:34 +0530

    Updated resume file

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,7 +57,7 @@ export default function Home() {
                   Resume <LinkArrow className={"w-6 ml-1"} />
                 </Link> */}
                 <a
-                  href="/suraj_ambekar_resume.pdf"
+                  href="/Suraj-Ambekar--Resume.pdf"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
